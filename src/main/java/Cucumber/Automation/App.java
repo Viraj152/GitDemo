cdf53fa08d6e86953bdf5e192247045f5ab27759
Viraj152 <viraj.jadhav@humancloud.biz>
2022-06-13 17:55:18 -0700

    Ensure feasibility

--- src/main/java/Cucumber/Automation/App.java
+++ src/main/java/Cucumber/Automation/App.java
@@ -14,5 +14,10 @@ public class App
         System.out.println("g");
         System.out.println("e");
         System.out.println("e=n ");
+        
+        // New code
+        System.out.println("Glow");
+        System.out.println("flow");
+        System.out.println("crow");
     }
 }
