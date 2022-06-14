9b5f4a5600be30d3a98b6b07e39c32f72f94afe2
Viraj152 <viraj.jadhav@humancloud.biz>
2022-06-13 18:44:17 -0700

    American Dev team

---
+++ src/main/java/Cucumber/Automation/Postman.java
@@ -0,0 +1,9 @@
+package Cucumber.Automation;
+
+public class Postman {
+public static void main(String[] args) {
+	System.out.println("hdvn");
+	System.out.println("jsb");
+	System.out.println("ayehn8");
+}
+}
