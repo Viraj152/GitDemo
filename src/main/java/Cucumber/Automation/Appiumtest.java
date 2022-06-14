a3a445397cdf662a5adf16bace6a30ea0aa72291
Viraj152 <viraj.jadhav@humancloud.biz>
2022-06-13 18:23:32 -0700

    Architect asian commit

---
+++ src/main/java/Cucumber/Automation/Appiumtest.java
@@ -0,0 +1,11 @@
+package Cucumber.Automation;
+
+public class Appiumtest {
+
+	public static void main(String[] args) {
+		System.out.println("hsag");
+		System.out.println("7q6rdh");
+		System.out.println("uetgfj");
+		System.out.println("hs3l");
+	}
+}
